0d7627acf366c9c8a652386204a241f5dcce291c
wentietie <wenatie@outlook.com>
2022-09-01 16:08:02 +0800

    0901

---
+++ docs/.vuepress/.temp/pages/notes/browser/storage.html.js
@@ -0,0 +1,27 @@
+export const data = {
+  "key": "v-40d5af0c",
+  "path": "/notes/browser/storage.html",
+  "title": "",
+  "lang": "zh-CN",
+  "frontmatter": {},
+  "excerpt": "",
+  "headers": [],
+  "git": {
+    "updatedTime": null,
+    "contributors": []
+  },
+  "filePathRelative": "notes/browser/storage.md"
+}
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
